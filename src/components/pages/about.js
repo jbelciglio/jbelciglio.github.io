1844e6b095e64fb3879ae267c87985039208ec79
jbelciglio <jbelciglio@gmail.com>
2019-01-19 16:32:12 -0800

    some progress

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,7 +9,8 @@ class About extends React.Component {
     render() {
         return (
             <div className="row">
-                <div class="card shadow-lg rounded" id="about">
+            <div className="col-md-6">
+                <div class="card shadow-lg rounded" id="years">
                     <div class="card-header">
                         A Life Spent in The Arts/Media
         </div>
@@ -22,18 +23,21 @@ class About extends React.Component {
                         </blockquote>
                     </div>
                 </div>
-                <div class="card shadow-lg rounded" id="about">
+                </div>
+                <div className="col-md-6">
+                <div class="card shadow-lg rounded" id="dreams">
                     <div class="card-header">
-                        A Life Spent in The Arts/Media
+                        Eyes on the Horizon
       </div>
                     <div class="card-body">
                         <blockquote class="blockquote mb-0">
-                            <p id="list">* 12 years Film Experience</p>
-                            <p id="list">* 4 years Game Experience</p>
-                            <p id="list">* 2 years Coding Experience</p>
+                            <p id="list">* Building new sites</p>
+                            <p id="list">* Brainstorming new ideas</p>
+                            <p id="list">* Breaking from the norms</p>
 
                         </blockquote>
                     </div>
+                </div>
                 </div>
             </div>
         )
